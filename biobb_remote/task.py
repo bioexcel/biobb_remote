dc26b35176c1d956c404a8d8c5cd4d1a0628fe4d
Josep Ll. Gelpi <gelpi@ub.edu>
2019-04-17 11:50:20 +0200

    Previous to discard

--- biobb_remote/task.py
+++ biobb_remote/task.py
@@ -1,10 +1,19 @@
 """ Base module to handle remote tasks """
 
+import os
+import base64
 
 class Task():
     """ Classe to handle task execution """
     def __init__(self, name, description):
+        self.id = base64.b64encode(os.urandom(8).decode('ascii'))
+        self.remote_id = ''
         self.name = name
         self.description = description
-
-    def execute(self, credentials):
+        self.ssh_data = sshCredentials()
+        self.script=''
+        
+    def launch(self, credentials_path)
+        self.ssh_data.load_from_file(credentials_path)
+        
+    
